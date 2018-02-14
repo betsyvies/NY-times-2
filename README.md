455f828612afc692d503f81f6d7143785d636442
betsyvies <ing.betsyvies@gmail.com>
2018-02-14 04:58:08 -0500

    añadiendo carpetas

---
+++ README.md
@@ -0,0 +1,27 @@
+# NY times
+
+## Objetivo
+
+- El objetivo de esta pagina es replicar la parte visual de la web LabCar, como tambien agregarle modales a los botones "Iniciar sesión", "Registrate", "Convierte en conductor", esto lo haremos con Bootstrap. 
+
+## Herramientas Utilizadas
+
+- Html, para la estructura del contenido.
+- Css, para darle estilo al contenido.
+- Bootstrap
+- Media queries.
+
+## Fuente consultada
+
+- [Laboratoria LMS] : https://lms.laboratoria.la/cohorts/lim-2017-09-bc-core-pm/courses/social-network/01-css-frameworks/02-bootstrap
+
+- [Bootstrap] : https://getbootstrap.com/docs/3.3/css/#forms
+
+- [W3School] : https://www.w3schools.com/bootstrap/bootstrap_forms.asp
+
+## Imagen 
+
+- Vista Mobile
+
+![LabCar](assets/images/mobile.png)
+
---
+++ public/js/app.js
@@ -0,0 +1,47 @@
+const formFetch = document.getElementById('search-form-fetch');
+const searchFieldFetch = document.getElementById('search-keyword-fetch');
+const responseContainerFetch = document.getElementById('response-container-fetch');
+
+formFetch.addEventListener('submit', function(e) {
+  e.preventDefault();
+  
+  /* Extraemos el valor del input */
+  responseContainerFetch.innerHTML = '';
+  let searchedForTextFetch = searchFieldFetch.value;
+  const url = `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForTextFetch}&api-key=347d672874fd431eb13860e1a3f4d50b`;
+
+  fetch(url)
+    .then(handleErrors)
+    .then(parseJSON)
+    .then(updateArticle)
+    .catch(displayErrors);
+});
+
+function handleErrors(res) {
+  if (!res.ok) {
+    throw Error(res.status);
+  }
+  return res;
+}
+
+function parseJSON(res) {
+  return res.json();
+}
+
+function updateArticle(data) {
+  console.log(data);
+  const article = data.response.docs[0];
+  const title = article.headline.main;
+  const snippet = article.snippet;
+
+  let li = document.createElement('li');
+  li.className = 'articleClass';
+  li.innerHTML = snippet;
+
+  responseContainerFetch.appendChild(li);
+}
+
+function displayErrors(err) {
+  console.log('INSIDE displayErrors!');
+  console.log(err);
+}
